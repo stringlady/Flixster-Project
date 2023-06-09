126eca28803130e9fdcd3f70192201bcde1aeef2
Anaya Bussey <anayabussey@gmail.com>
2023-06-08 23:14:17 -0400

    All Core Feature Implemented

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ var page = 1;
 const baseURL = 'https://api.themoviedb.org/3';
 const display = document.querySelector("#display");
 const loadButton = document.querySelector(".loading");
+const newLoad = document.querySelector(".loading_2");
 const search_input = document.getElementById("search-input");
+const search_form = document.getElementById("searchForm");
 const x_button = document.getElementById("x-button");
+const label = document.getElementById("label");
+const body = document.querySelector("body");
 
 /**
- * $page=
+ *
  * Update the DOM to display results from the Giphy API query.
  *
  * @param {Object} results - An array of results containing each item
@@ -31,6 +35,38 @@ function xAppear() {
     if(x_button.classList.contains("hide") === true) {
         x_button.classList.remove("hide");
     }
+    loadButton.classList.add("hide");
+}
+
+//Now Playing
+/**
+ * Make the actual `fetch` request to the Movie API
+ * and appropriately handle the response.
+ */
+async function getMovieApiResults() {
+    const fetchURL = `${baseURL}/movie/now_playing?api_key=${api_key}&page=${page}`;
+    const response = await fetch(fetchURL);
+    const data = await response.json();
+    data.results.forEach(element => {
+        displayResults(element);
+    });
+    
+}
+
+//Search Query
+/**
+ * Make the actual `fetch` request to the Movie API
+ * and appropriately handle the response.
+ */
+async function getSearchMovieApiResults(searchTerm) {
+    const fetchURL = `${baseURL}/search/movie?api_key=${api_key}&query=${searchTerm}&page=${page}`;
+    const response = await fetch(fetchURL);
+    const data = await response.json();
+    data.results.forEach(element => {
+        displayResults(element);
+    });
+    label.innerText = `Search Query = "${searchTerm}"`;
+    
 }
 
 //Now Playing
@@ -54,6 +90,54 @@ async function getMovieApiResults() {
  * @param {SubmitEvent} event - The SubmitEvent triggered when submitting the form
  *
  */
+function xButtonWorking() {
+    page = 1;
+    display.innerHTML = '';
+    getMovieApiResults();
+    x_button.classList.add("hide");
+    label.innerText = "Now Playing";
+    if(loadButton.classList.contains("hide") === true) {
+        loadButton.classList.remove("hide");
+    }
+    if(newLoad.classList.contains("hide") === false) {
+        newLoad.classList.add("hide");
+    }
+}
+
+/**
+ * The function responsible for handling all form submission events.
+ *
+ * @param {SubmitEvent} event - The SubmitEvent triggered when submitting the form
+ *
+ */
+async function handleSearch(event) {
+    page = 1;
+    event.preventDefault();
+    display.innerHTML = '';
+    getSearchMovieApiResults(search_input.value);
+    newLoad.classList.remove("hide");
+}
+
+/**
+ * The function responsible for handling all form submission events.
+ *
+ * @param {SubmitEvent} event - The SubmitEvent triggered when submitting the form
+ *
+ */
+async function handleSearchShowMore(event) {
+    page += 1;
+    event.preventDefault();
+    getSearchMovieApiResults(search_input.value);
+    //console.log(fetchURL);
+    //console.log(page);
+}
+
+/**
+ * The function responsible for handling all form submission events.
+ *
+ * @param {SubmitEvent} event - The SubmitEvent triggered when submitting the form
+ *
+ */
 async function handleShowMore(event) {
     page += 1;
     event.preventDefault();
@@ -63,7 +147,12 @@ async function handleShowMore(event) {
 }
 
 window.onload = function () {
+    newLoad.classList.add("hide");
     getMovieApiResults();
     loadButton.addEventListener("submit", handleShowMore);
-    search_input.addEventListener("click", xAppear);
+    //search_input.addEventListener("click", xAppear);
+    search_form.addEventListener("submit", handleSearch);
+    search_form.addEventListener("submit", xAppear);
+    x_button.addEventListener("click", xButtonWorking);
+    newLoad.addEventListener("submit", handleSearchShowMore);
 }
